220c2704d4cb19b4c10819a715f814a2206d152f
髙草木 和史 <kazushi.takakusagi@gmail.com>
2024-10-15 17:55:35 +0900

    feat: tool_choice

--- src/function-calling/main.py
+++ src/function-calling/main.py
@@ -7,42 +7,48 @@ client = OpenAI()
 
 MODEL_TYPE = "gpt-4o"
 
-FUNCTIONS_LIST = [
+TOOLS_LIST = [
     {
-        "name": "get_weather",
-        "description": "指定された場所と日付の天気を取得する",
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "location": {
-                    "type": "string",
-                    "description": "場所を指定する",
+        "type": "function",
+        "function": {
+            "name": "get_weather",
+            "description": "指定された場所と日付の天気を取得する",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "location": {
+                        "type": "string",
+                        "description": "場所を指定する",
+                    },
+                    "date": {
+                        "type": "string",
+                        "description": "日付を指定する",
+                    },
                 },
-                "date": {
-                    "type": "string",
-                    "description": "日付を指定する",
-                },
+                "required": ["location", "date"],
             },
-            "required": ["location", "date"],
         },
     },
     {
-        "name": "write_spreadsheet",
-        "description": "スプレッドシートにデータを書き込む",
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "data": {
-                    "type": "string",
-                    "description": "データを指定する",
+        "type": "function",
+        "function": {
+            "name": "write_spreadsheet",
+            "description": "データをスプレッドシートに書き込む",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "data": {
+                        "type": "string",
+                        "description": "データを指定する",
+                    },
+                    "sheet_name": {
+                        "type": "string",
+                        "description": "シート名を指定する",
+                        "default": "Sheet1",
+                    },
                 },
-                "sheet_name": {
-                    "type": "string",
-                    "description": "シート名を指定する",
-                    "default": "Sheet1",
-                },
+                "required": ["data", "sheet_name"],
             },
-            "required": ["data", "sheet_name"],
         },
     },
 ]
@@ -54,30 +60,37 @@ def main(user_question: str):
     response = client.chat.completions.create(
         model=MODEL_TYPE,
         messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
+            # システムメッセージ
+            {
+                "role": "system",
+                "content": "You are a helpful assistant. You are responsible for writing results to spreadsheets and retrieving data.",
+            },
+            # ユーザーメッセージ
             {"role": "user", "content": user_question},
         ],
-        functions=FUNCTIONS_LIST,
-        function_call="auto",
+        tools=TOOLS_LIST,
+        tool_choice="auto",
     )
-    # function_call を判定する
-    function_call = response.choices[0].message.function_call
-    print(function_call)
-    if function_call.name == "get_weather":
-        # 関数を呼び出す
-        args = json.loads(function_call.arguments)
-        location = args.get("location")
-        date = args.get("date")
-        weather = get_weather(location, date)
-        return weather
-    elif function_call.name == "write_spreadsheet":
-        args = json.loads(function_call.arguments)
-        data = args.get("data")
-        sheet_name = args.get("sheet_name")
-        write_spreadsheet(data, sheet_name)
-        return "スプレッドシートにデータを書き込みました"
-    else:
-        return response
+    tool_calls = response.choices[0].message.tool_calls
+    print("tool_calls:", tool_calls)
+    for tool_call in tool_calls:
+        print("tool_call:", tool_call.function.name)
+        if tool_call.function.name == "get_weather":
+            # 関数を呼び出す
+            args = json.loads(tool_call.function.arguments)
+            location = args.get("location")
+            date = args.get("date")
+            weather = get_weather(location, date)
+            print("get_weather:", weather)
+        elif tool_call.function.name == "write_spreadsheet":
+            args = json.loads(tool_call.function.arguments)
+            data = args.get("data")
+            sheet_name = args.get("sheet_name")
+            result = write_spreadsheet(data, sheet_name)
+            print("write_spreadsheet:", result)
+        else:
+            return response.choices[0].message.content
+    return response
 
 
 def get_weather(location: str, date: str) -> str:
@@ -94,6 +107,8 @@ if __name__ == "__main__":
     # ユーザーの質問を受け取る
     # user_question = input("質問を入力してください: ")
     # user_question = "今日の東京の天気はどうですか？"
-    user_question = "スプレッドシートに[東京, 大阪, 札幌]のデータを書き込む"
+    # user_question = "スプレッドシートに[東京, 大阪, 札幌]のデータを書き込む"
+    # 2つの関数を呼び出す
+    user_question = "今日の東京の天気を取得してください。その結果をスプレッドシートに書き込んでください。"
     response = main(user_question)
-    print(response)
+    # print(response)
